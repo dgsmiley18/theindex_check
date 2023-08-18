cb0ab3cfea7f4c588a684c2b724ba37f25fd2ee8
dgsmiley18 <douglasvianna230@gmail.com>
2023-08-17 21:22:15 -0300

    refactor: :recycle: Code refactoring and continuous scraping
    Code was refatored, cleaning urls in few lines, now the script is continuous, it will analyze all libraries

--- brokensite.py
+++ brokensite.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from ping3 import ping
 import os
 import time
-
 option = webdriver.ChromeOptions()
 option.add_argument('--headless')
 option.add_argument('--no-sandbox')
@@ -14,108 +13,109 @@ option.add_argument('--disable-dev-sh-usage')
 # Start the chrome driver
 driver = webdriver.Chrome(options=option)
 
- # Start reading all the links from the file
-with open("links.txt", "r") as reader:
-    working = 0
-    not_working = 0
-    false_positive = 0
+def brokensite():
+    # Start reading all the links from the file
+    with open("links.txt", "r") as reader:
+        working = 0
+        not_working = 0
+        false_positive = 0
 
-    for line in reader.readlines():
-        print(f"Analyzing: {line}")
-        url = "https://www.isitdownrightnow.com/{}.html".format(line.strip())
+        for line in reader.readlines():
+            print(f"Analyzing: {line}")
+            url = "https://www.isitdownrightnow.com/{}.html".format(line.strip())
 
-        driver.get(url)
+            driver.get(url)
 
-        # catch the updated html
-        html = driver.page_source
+            # catch the updated html
+            html = driver.page_source
 
-        soup = BeautifulSoup(html, "lxml")
-        time.sleep(5)
-        try:
-            site_status = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,'/html/body/div[2]/div[2]/div[1]/div[3]/div[5]/div'))).text
-        except Exception as e:
-            site_status = "troll"
-            print("Not found, reading the title...")
-        title = soup.find("title").get_text()
-        
-        # If the website is UP
-        if "UP and reachable by us" in site_status:
+            soup = BeautifulSoup(html, "lxml")
+            time.sleep(5)
             try:
-                status_text = site_status
-                site_name = soup.select_one("div.tabletr:nth-child(1) > span:nth-child(1)").get_text()
-                url_checked = soup.select_one("div.tabletrsimple:nth-child(2) > span:nth-child(1)").get_text()
-                response_time = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[3]/div[3]/span'))).text
-                last_down = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[3]/div[4]/span'))).text
-
-                with open("websites_tested.txt", "a") as writer:
-                    writer.write(f"Site: {site_name}\n")
-                    writer.write(f"URL: {url_checked}\n")
-                    writer.write(f"Response Time: {response_time}\n")
-                    writer.write(f"Last Down: {last_down}\n")
-                    writer.write(f"Status: {status_text}\n")
-                    writer.write("-------------------------------------------------\n")
-
-                print("The file was created successfully!")
-                # If the site is up, add +1 to the counter
-                working += 1
+                site_status = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,'/html/body/div[2]/div[2]/div[1]/div[3]/div[5]/div'))).text
             except Exception as e:
-                print("Error: ", e)
+                site_status = "troll"
+                print("Not found, reading the title...")
+            title = soup.find("title").get_text()
+            
+            # If the website is UP
+            if "UP and reachable by us" in site_status:
+                try:
+                    status_text = site_status
+                    site_name = soup.select_one("div.tabletr:nth-child(1) > span:nth-child(1)").get_text()
+                    url_checked = soup.select_one("div.tabletrsimple:nth-child(2) > span:nth-child(1)").get_text()
+                    response_time = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[3]/div[3]/span'))).text
+                    last_down = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[3]/div[4]/span'))).text
 
-        # If the website is DOWN
-        elif "is declined" in site_status or "DOWN for everyone" in site_status:
-            try:
-                status_text = site_status
-                site_name = soup.select_one("div.tabletr:nth-child(1) > span:nth-child(1)").get_text()
-                url_checked = soup.select_one("div.tabletrsimple:nth-child(2) > span:nth-child(1)").get_text()
-                response_time = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[3]/div[3]/span'))).text
-                last_down = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[3]/div[4]/span'))).text
-
-                with open("websites_tested.txt", "a") as writer:
-                    writer.write(f"Site: {site_name}\n")
-                    writer.write(f"URL: {url_checked}\n")
-                    writer.write(f"Response Time: {response_time}\n")
-                    writer.write(f"Last Down: {last_down}\n")
-                    writer.write(f"Status: {status_text}\n")
-                    writer.write(f"{'-' * 49}\n")
-
-                print("The file was created successfully!")
-                # If the site is down, add +1 to the counter
-                not_working += 1
-            except Exception as e:
-                print("Error: ", e)
-        # if the isitdownrightnow.com is broken or website is not valid
-        elif title == "Is It Down Right Now - 404 Not Found" or "Enter a domain below" in title:
-            try:
-                hostname = line.strip()
-                # Ping the website
-                response_time = ping(hostname)
-
-                if response_time is not None:
-                    status = 'UP'
-                    working += 1
-                else:
-                    status = 'DOWN'
-                    not_working += 1
-
-                result = f"{hostname} is {status}"
-
-                with open("websites_tested.txt", "a") as writer:
-                        writer.write(f"URL: {hostname}\n")
-                        writer.write(f"Status: {result}\n")
+                    with open("websites_tested.txt", "a") as writer:
+                        writer.write(f"Site: {site_name}\n")
+                        writer.write(f"URL: {url_checked}\n")
+                        writer.write(f"Response Time: {response_time}\n")
+                        writer.write(f"Last Down: {last_down}\n")
+                        writer.write(f"Status: {status_text}\n")
                         writer.write("-------------------------------------------------\n")
-                print("Added to the file!")
-            except Exception as e:
-                print("Error: ", e)
-        # if the URL is invalid
-        else:
-            with open("urls_ignored.txt", "a") as writer:
-                writer.write(f"URL: {line.strip()}\n")
-                writer.write("This URL is invalid or cannot be checked\n")
-                writer.write("-------------------------------------------------\n")
-                # If the site is invalid or cloudflare problem, add +1 to the counter
-                false_positive += 1
 
-# close the driver, clean the console and show the result            
-driver.quit()
-os.system("cls")
-print(f"Working: {working}\nNot Working: {not_working}\nFalse Positive: {false_positive}")
+                    print("The file was created successfully!")
+                    # If the site is up, add +1 to the counter
+                    working += 1
+                except Exception as e:
+                    print("Error: ", e)
+
+            # If the website is DOWN
+            elif "is declined" in site_status or "DOWN for everyone" in site_status:
+                try:
+                    status_text = site_status
+                    site_name = soup.select_one("div.tabletr:nth-child(1) > span:nth-child(1)").get_text()
+                    url_checked = soup.select_one("div.tabletrsimple:nth-child(2) > span:nth-child(1)").get_text()
+                    response_time = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[3]/div[3]/span'))).text
+                    last_down = WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div[3]/div[4]/span'))).text
+
+                    with open("websites_tested.txt", "a") as writer:
+                        writer.write(f"Site: {site_name}\n")
+                        writer.write(f"URL: {url_checked}\n")
+                        writer.write(f"Response Time: {response_time}\n")
+                        writer.write(f"Last Down: {last_down}\n")
+                        writer.write(f"Status: {status_text}\n")
+                        writer.write(f"{'-' * 49}\n")
+
+                    print("The file was created successfully!")
+                    # If the site is down, add +1 to the counter
+                    not_working += 1
+                except Exception as e:
+                    print("Error: ", e)
+            # if the isitdownrightnow.com is broken or website is not valid
+            elif title == "Is It Down Right Now - 404 Not Found" or "Enter a domain below" in title:
+                try:
+                    hostname = line.strip()
+                    # Ping the website
+                    response_time = ping(hostname)
+
+                    if response_time is not None:
+                        status = 'UP'
+                        working += 1
+                    else:
+                        status = 'DOWN'
+                        not_working += 1
+
+                    result = f"{hostname} is {status}"
+
+                    with open("websites_tested.txt", "a") as writer:
+                            writer.write(f"URL: {hostname}\n")
+                            writer.write(f"Status: {result}\n")
+                            writer.write("-------------------------------------------------\n")
+                    print("Added to the file!")
+                except Exception as e:
+                    print("Error: ", e)
+            # if the URL is invalid
+            else:
+                with open("urls_ignored.txt", "a") as writer:
+                    writer.write(f"URL: {line.strip()}\n")
+                    writer.write("This URL is invalid or cannot be checked\n")
+                    writer.write("-------------------------------------------------\n")
+                    # If the site is invalid or cloudflare problem, add +1 to the counter
+                    false_positive += 1
+
+    # close the driver, clean the console and show the result            
+    driver.quit()
+    os.system("cls")
+    print(f"Working: {working}\nNot Working: {not_working}\nFalse Positive: {false_positive}")
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import Select
 from PIL import Image
 from urllib.parse import urlparse
 import time
+import brokensite
 
 logging.basicConfig(level=logging.ERROR)
 option = webdriver.ChromeOptions()
@@ -16,80 +17,89 @@ option.add_argument("--disable-dev-sh-usage")
 # Setup selenium to use a chrome
 driver = webdriver.Chrome(options=option)
 
-# Load the page with selenium
-driver.get("https://theindex.moe/library/anime")
-time.sleep(2)
-# Get the complete HTML from the page
-select_element = Select(
-    driver.find_element(By.CSS_SELECTOR, "select.Input_input___QkW_:nth-child(1)")
-)
-select_element.select_by_index(3)
-time.sleep(10)
+urls_index = [
+    "anime",
+    "hentai",
+    "manga",
+    "novels"
 
-# Get the width and height of the page (Page does not loads completely)
-width = driver.execute_script(
-    "return Math.max(document.body.scrollWidth, document.documentElement.scrollWidth, document.body.offsetWidth, document.documentElement.offsetWidth, document.documentElement.clientWidth);"
-)
-height = driver.execute_script(
-    "return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.documentElement.clientHeight);"
-)
+]
 
-# Set browser window dimensions to span the entire page (so it can scrape all the links)
-driver.set_window_size(width, height)
+for index, library in enumerate(urls_index):
 
-# Take a full screenshot
-screenshot = driver.get_screenshot_as_png()
+    # Load the page with selenium
+    driver.get(f"https://theindex.moe/library/{library}")
+    time.sleep(5)
+    # Get the complete HTML from the page
+    
+    select_element = Select(
+        driver.find_element(By.XPATH, "/html/body/div/div/div[1]/main/div[5]/div/select")
+    )
+    select_element.select_by_index(3)
+    time.sleep(5)
 
-# Take a screenshoot with PIL
-image = Image.open(io.BytesIO(screenshot))
+    # Get the width and height of the page (Page does not loads completely)
+    width = driver.execute_script(
+        "return Math.max(document.body.scrollWidth, document.documentElement.scrollWidth, document.body.offsetWidth, document.documentElement.offsetWidth, document.documentElement.clientWidth);"
+    )
+    height = driver.execute_script(
+        "return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.documentElement.clientHeight);"
+    )
 
-image.save("screenshot.png")
+    # Set browser window dimensions to span the entire page (so it can scrape all the links)
+    driver.set_window_size(width, height)
 
-# Update the HTML with the new HTML
-html = driver.execute_script("return document.documentElement.outerHTML")
-# Close the selenium for save resources
-driver.quit()
+    # Take a full screenshot
+    screenshot = driver.get_screenshot_as_png()
 
-# Give the HTML to BS for analyse
-soup = BeautifulSoup(html, "html.parser")
-count = 0
-divs = soup.find("div", class_="d-flex flex-wrap my-2").find_all(
-    "div", class_="Card_card__pf4Rb card bg-2 mb-2 me-2"
-)
+    # Take a screenshoot with PIL
+    image = Image.open(io.BytesIO(screenshot))
 
-# Starts to browse all registered websites
+    image.save("screenshot.png")
 
-for div in divs:
-    count += 1
-    try:
-        with open("links.txt", "a") as writer:
-            link = (
-                div.find("div", class_="row g-0 h-100")
-                .find("div", class_="card-body d-flex flex-column p-2 h-100")
-                .find("h5", class_="Card_title__6erSA card-title")
-                .find("a")
-                .get("href")
-            )
+    # Update the HTML with the new HTML
+    html = driver.execute_script("return document.documentElement.outerHTML")
+    # # Close the selenium for save resources
+    # driver.quit()
 
-            # if the link is not github.com, parse the url to remove https and /
-            # Example: itazuraneko.neocities.org
-            if "github.com" not in link:
-                parsed_url = urlparse(link)
-                site_link = parsed_url.netloc
-                if site_link.startswith("www."):
-                    site_link = site_link[4:]
-                writer.write(site_link + "\n")
-                continue
+    # Give the HTML to BS for analyse
+    soup = BeautifulSoup(html, "html.parser")
+    count = 0
+    divs = soup.find("div", class_="d-flex flex-wrap my-2").find_all(
+        "div", class_="Card_card__X0xjX card bg-2 mb-2 me-2"
+    )
 
-            link = link.replace("https://", "")
-            link = link.replace("http://", "")
-            link = link.replace("www.", "")
+    # Starts to browse all registered websites
 
-            if link.endswith("/"):
-                link = link[:-1]
-            writer.write(link + "\n")
-        print("New link added to links.txt!")
-    except Exception as e:
-        print("Error: ", e)
+    for div in divs:
+        count += 1
+        try:
+            with open("links.txt", "a") as writer:
+                link = (
+                    div.find("div", class_="row g-0 h-100")
+                    .find("div", class_="card-body d-flex flex-column p-2 h-100")
+                    .find("h5", class_="Card_title__8lWUm card-title")
+                    .find("a")
+                    .get("href")
+                )
 
-print(f"Websites found on TheIndex: {count}")
+                # parse the url to remove https and /
+                # Example: itazuraneko.neocities.org
+                parsed_url = urlparse(link).netloc
+                writer.write(parsed_url + "\n")
+
+            print("New link added to links.txt!")
+        except Exception as e:
+            print("Error: ", e)
+
+    print(f"Websites found on TheIndex in the library {library}: {count}")
+    if index < len(urls_index) - 1:
+        next_library = urls_index[index + 1]
+        print(f"Current library: {library}, Next library: {next_library}") 
+        print("Waiting 10 seconds before the next library")
+        time.sleep(5)
+    else:
+        driver.quit()
+        print("All websites collected, analysing them...")
+
+brokensite()
