3840c302dee79e97df2a6d92bf12ef2357438ed5
Douglas Vianna <douglasvianna230@gmail.com>
2023-11-08 22:15:24 -0300

    refactor: :recycle: Refactor code to store items in JSON file
    The code will no longer use Selenium to get the urls, it will now use Requests which will be more fast

--- theindex_check/find_websites.py
+++ theindex_check/find_websites.py
@@ -1,10 +1,21 @@
 import requests
 import json
 
-
-r = requests.get(f"https://theindex.moe/_next/data/WR6kC01BpbK8Nyw4ytHn_/items.json")
+r = requests.get("https://theindex.moe/_next/data/WR6kC01BpbK8Nyw4ytHn_/items.json")
 response = r.json()
 items = response["pageProps"]["items"]
+
+# List to store the items
+lista_items = []
+
 for item in items:
-    print(item["name"])
-    print(item["urls"][0])
+    dictionary = {
+        "name": item["name"],
+        "url":  item["urls"][0]
+    }
+
+    lista_items.append(dictionary)
+
+# It will store all the items in a JSON file
+with open("items.json", 'w') as file:
+    json.dump(lista_items, file, indent=4)
